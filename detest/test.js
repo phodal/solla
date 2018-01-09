5ae590c82015094f18b72180ac7cfd41d566c2c5
Phodal HUANG <h@phodal.com>
2018-01-09 08:06:51 +0800

    [BG] it seems can be work

--- detest/test.js
+++ detest/test.js
@@ -1,4 +1,4 @@
 var Trianglify = require('trianglify'); // only needed in node.js
 var pattern = Trianglify({width: 200, height: 200})
 
-console.log(pattern.svg());
+console.log(pattern.svg().innerHTML);

4ac860427d6fd9c8bad94e3a33b37767a0e66d32
Phodal HUANG <h@phodal.com>
2018-01-09 08:10:58 +0800

    [BG] dotest: add color func test

--- detest/test.js
+++ detest/test.js
@@ -1,4 +1,12 @@
 var Trianglify = require('trianglify'); // only needed in node.js
-var pattern = Trianglify({width: 200, height: 200})
 
-console.log(pattern.svg().innerHTML);
+var colorFunc = function(x, y) {
+  return 'hsl('+Math.floor(Math.abs(x*y)*360)+',80%,60%)';
+};
+var pattern = Trianglify({
+  color_function: colorFunc,
+  width: 200,
+  height: 200
+})
+
+console.log('<svg width="2370" height="2500" viewBox="0 0 256 270" xmlns="http://www.w3.org/2000/svg" preserveAspectRatio="xMinYMin meet">' + pattern.svg().innerHTML + '</svg>');

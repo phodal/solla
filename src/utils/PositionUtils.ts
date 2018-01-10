84cbe7b9184a02b2225533d94eccfe2bf91e2d01
Phodal HUANG <h@phodal.com>
2018-01-10 21:31:44 +0800

    [pos] update back arth

--- src/utils/PositionUtils.ts
+++ src/utils/PositionUtils.ts
@@ -2,16 +2,23 @@ import SOLLA_CONFIG from '../utils/contants'
 
 let PositionUtils = {
   getPosition: (size: number, index: number): SollaPosition => {
+    let defaultX = SOLLA_CONFIG.WIDTH / 2 - SOLLA_CONFIG.ICON.WIDTH / 2
+    let defaultY = SOLLA_CONFIG.HEIGHT / 2 - SOLLA_CONFIG.ICON.HEIGHT / 2
+
     if (size === 1) {
       return {
-        x: SOLLA_CONFIG.WIDTH / 2 - SOLLA_CONFIG.ICON.WIDTH / 2,
-        y: SOLLA_CONFIG.HEIGHT / 2 - SOLLA_CONFIG.ICON.HEIGHT / 2
+        x: defaultX,
+        y: defaultY
       }
     }
 
+    function getXPosition () {
+      return SOLLA_CONFIG.WIDTH / (2 + size - 1) * (index + 1) - SOLLA_CONFIG.ICON.WIDTH / 2
+    }
+
     return {
-      x: SOLLA_CONFIG.WIDTH / (2 + index) - SOLLA_CONFIG.ICON.WIDTH / 2,
-      y: SOLLA_CONFIG.HEIGHT / (2 + index) - SOLLA_CONFIG.ICON.HEIGHT / 2
+      x: getXPosition(),
+      y: defaultY
     }
   }
 }

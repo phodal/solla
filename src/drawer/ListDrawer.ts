b5d3700ef369e561d3b1e1b1a7f210bd1bcbfbff
Phodal HUANG <h@phodal.com>
2018-01-16 07:59:22 +0800

    [LIST] create base drawer

---
+++ src/drawer/ListDrawer.ts
@@ -0,0 +1,11 @@
+import BaseSVGDrawer from './BaseSVGDrawer'
+
+export default class ListDrawer extends BaseSVGDrawer {
+  init (): any {
+    return undefined
+  }
+
+  draw (): any {
+    return ''
+  }
+}
